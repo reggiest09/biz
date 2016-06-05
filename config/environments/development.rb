15348b03f6034e2553272ab2e0ee78bb2a26b299
Reggie <reggiest09@gmail.com>
2016-06-04 22:46:51 -0400

    final before heroku push

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,21 @@ config.action_mailer.smtp_settings =  {
   :domain         => 'gmail.com',
   :enable_starttls_auto => true
 }
+
+#paperclip upload
+config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
+  }
+}
+
+
+
+
+
+
 end
